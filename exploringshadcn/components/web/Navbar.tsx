359015fc557bcf0e2f55e519e6b503ce1f510ee3
Deepan Patel <55546591+deepan-patel@users.noreply.github.com>
2026-04-01 05:28:22 -0400

    Created Sidebar
    -- added side bar comp
    
    -- added cookies to control default state on web refresh
    
    --  added nested items/collapsable
    
    -- added side bar group action for short cut to forms

--- exploringshadcn/components/web/Navbar.tsx
+++ exploringshadcn/components/web/Navbar.tsx
@@ -2,11 +2,12 @@ import { ThemeToggle } from "../theme/ThemeToggle";
 
 import Link from "next/link";
 import AvatarComponent from "./AvatarComponent";
+import { SidebarTrigger } from "@/components/ui/sidebar"
 
 export default function Navbar() {
     return (
         <nav className="flex flex-row justify-between p-4 items-center">
-            <div>Collapable</div>
+            <SidebarTrigger />
             <div className="flex flex-row items-center gap-4">
                 <Link href="/dashboard">Dashboard</Link>
                 <ThemeToggle />
